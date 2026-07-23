b29d6623f7574e796b7bd068e2ce68943a6d027c
vladalexkirbut2003@gmail.com <Vladislav>
2026-07-23 23:09:35 +0300

    JAC-6 [User Service] Caching

---
+++ src/main/java/com/innowise/userservice/util/CacheNames.java
@@ -0,0 +1,10 @@
+package com.innowise.userservice.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CacheNames {
+
+    public static final String USERS = "users";
+
+}
